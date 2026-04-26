3b106644f34ef2ce755a2e7753737aea209b90cc
creomobile-msi <creomobile@gmail.com>
2026-04-26 20:27:36 +0200

    add base integration tests

---
+++ tests/Creomobile.Data.EFCore.IntegrationTests/PostgresFixture.cs
@@ -0,0 +1,18 @@
+﻿using Testcontainers.PostgreSql;
+using Testcontainers.Xunit;
+using Xunit.Sdk;
+
+namespace Creomobile.Data.EFCore.IntegrationTests;
+
+public sealed class PostgresFixture(IMessageSink sink) : ContainerFixture<PostgreSqlBuilder, PostgreSqlContainer>(sink)
+{
+    const string Db = "tests";
+    const string User = "postgres";
+    const string Password = "postgres";
+
+    protected override PostgreSqlBuilder Configure() => new PostgreSqlBuilder("postgres:18.3")
+        .WithDatabase(Db)
+        .WithUsername(User)
+        .WithPassword(Password)
+        .WithPortBinding(5432, true);
+}
---
+++ tests/Creomobile.Data.EFCore.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿namespace Creomobile.Data.EFCore.IntegrationTests;
+
+public sealed class UnitTest1(PostgresFixture postgresFixture) : IClassFixture<PostgresFixture>
+{
+    [Fact]
+    public void Test1()
+    {
+        var i1 = 123;
+        var i2 = 321;
+
+        var sum = i1 + i2;
+
+        sum.Should().Be(444);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var cs = postgresFixture.Container.GetConnectionString();
+
+    }
+}
