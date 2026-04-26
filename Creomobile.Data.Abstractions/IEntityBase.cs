4170c89b07715cc7c8910aac4ebbf8e4e7725a86
creomobile-msi <creomobile@gmail.com>
2026-04-26 15:05:18 +0200

    Add project files.

---
+++ Creomobile.Data.Abstractions/IEntityBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Creomobile.Data.Abstractions;
+
+/// <summary>
+/// Abstraction for an entity exposing a strongly-typed primary key.
+/// </summary>
+/// <typeparam name="TId">Primary key type (e.g. <see cref="System.Guid"/>, <see cref="int"/>, <see cref="string"/>).</typeparam>
+public interface IEntityBase<TId> where TId : notnull
+{
+    /// <summary>
+    /// Primary key value. For new (not yet persisted) entities this may be the default value of <typeparamref name="TId"/>.
+    /// </summary>
+    TId Id { get; set; }
+}
